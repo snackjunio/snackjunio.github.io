9e3cbc3bfdbe636cebd0d01f1fce8acfd86a2fa1
unknown <jun.nguyen1761993@gmail.com>
2020-04-15 00:00:50 +0900

    Second commit

--- cms/urls.py
+++ cms/urls.py
@@ -12,4 +12,9 @@ urlpatterns = [
     path('book/mod/<int:book_id>/', views.book_edit, name='book_mod'),
     # 本を削除
     path('book/del/<int:book_id>/', views.book_del, name='book_del'),
+    # Impression
+    path('impression/<int:book_id>/', views.ImpressionList.as_view(), name='impression_list'),
+    path('impression/add/<int:book_id>/', views.impression_edit, name='impression_add'),
+    path('impression/mod/<int:book_id>/<int:impression_id>/', views.impression_edit, name='impression_mod'),
+    path('impression/del/<int:book_id>/<int:impression_id>/', views.impression_del, name='impression_del'),
 ]
