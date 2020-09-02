fa4e05794f8c5d84b01c8a77e926649f18de061a
Nguyen <nguyen-duc-hiep@tbrave.com>
2020-09-02 15:43:33 +0900

    leetcode追加
    Signed-off-by: Nguyen <nguyen-duc-hiep@tbrave.com>

---
+++ LeetCode/ImplementstrStr().py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if len(needle) == 0:
+            return 0
+        elif needle in haystack:
+            return haystack.index(needle)
+        else:
+            return -1
--- LeetCode/RemoveDuplicatesfromSortedArray.py
+++ LeetCode/RemoveDuplicatesfromSortedArray.py
@@ -1,8 +1,18 @@
-
-def main(nums):
-    
-    return list(set(nums))
-
-if __name__ == "__main__":
-    nums =[1,1,2]
-    main(nums)
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) <= 1:
+            return len(nums)
+        i = 0
+        j = i+1
+        while j < len(nums):
+            if nums[j] == nums[i]:
+                j += 1
+            else:
+                nums[i+1] = nums[j]
+                i += 1
+        return i+1
+        
